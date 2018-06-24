47e3cc7f6195fc017eb3b74dae9fcf9252d72a1a
DryoX <40540721+DryoX@users.noreply.github.com>
2018-06-25 00:29:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.TOKEN = 'NDYwMDgyMzEyMjIzMzkxNzU0.DhEjqQ.ORaW3fgqNCF7z9g0-DCg8E2C32A';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = '!!';
 
 exports.GOOGLE_API_KEY = 'AIzaSyAL5dbalHx8PKEjqhiu5ApTgd4ZV4aqcVw';
